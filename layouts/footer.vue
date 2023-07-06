08fca5684a409efa2c92d320e8cccf1f9a26a968
hengchomroeunnn <hengchomroeun3103@gmail.com>
2023-07-06 15:29:50 +0700

    footer-p4

--- layouts/footer.vue
+++ layouts/footer.vue
@@ -3,7 +3,9 @@
     class="p-2 flex flex-wrap justify-between border-t-4 pb-4 pt-4 max-[515px]:items-center max-[515px]:flex max-[515px]:justify-center">
     <div class="">
       <div class="w-[40px] max-[400px]:w-[30px]">
-        <nuxt-link to="/"><img src="/images/logo.png" alt="Heng Chomroeun" /></nuxt-link>
+        <nuxt-link to="/"
+          ><img src="/images/logo.png" alt="Heng Chomroeun"
+        /></nuxt-link>
       </div>
       <h1 class="text-lg font-bca text-gray-500 max-[400px]:text-base">
         Heng chomroeun, 2023.
@@ -37,7 +39,7 @@
     </div>
   </div>
   <div
-    class="flex flex-wrap justify-between font-bca text-gray-500 border-t-4 border-[#7ED957] pt-4 max-md:text-sm max-sm:text-xs max-[515px]:items-center max-[515px]:flex max-[515px]:justify-center">
+    class="p-4 flex flex-wrap justify-between font-bca text-gray-500 border-t-4 border-[#7ED957] pt-4 max-md:text-sm max-sm:text-xs max-[515px]:items-center max-[515px]:flex max-[515px]:justify-center">
     <div>© 2023 Heng Chomroeun, All rights reserved.</div>
     <div class="flex flex-wrap space-x-16 max-md:space-x-8 max-sm:space-x-4">
       <p>Term of Service</p>
